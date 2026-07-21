f9bf5b59a980fe2f1c8e439dfbcae6be2c61a18c
rizkiismail <rizkiandriana17@gmail.com>
2026-07-21 10:09:58 +0700

    tambah footer

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -58,6 +58,12 @@
     @yield('content')
 </div>
 
+<footer class="footer py-3 bg-light border-top">
+    <div class="container-fluid px-4">
+        <div class="text-center small text-muted">© 2026 Rizki Andriana Ismail</div>
+    </div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 @yield('scripts')
 </body>
